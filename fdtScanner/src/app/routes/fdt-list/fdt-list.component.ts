88f840c54ff4d292d8b37fce5b66794cb3f8185a
Merseni Bilel <bilelmerseni7016@gmail.com>
2022-05-07 19:41:55 +0100

    update fdt

--- fdtScanner/src/app/routes/fdt-list/fdt-list.component.ts
+++ fdtScanner/src/app/routes/fdt-list/fdt-list.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Fdtlist,
+  FdtListService,
+} from 'src/app/services/getFdtList/fdt-list.service';
 
 @Component({
   selector: 'app-fdt-list',
   templateUrl: './fdt-list.component.html',
-  styleUrls: ['./fdt-list.component.less']
+  styleUrls: ['./fdt-list.component.less'],
 })
 export class FdtListComponent implements OnInit {
+  fdtlist: Fdtlist[] = [];
+  isLoading = false;
 
-  constructor() { }
+  // modal
+  visible = false;
+
+  // form
+  fdtUpdateFrom! : FormGroup ;
+
+  constructor(
+    private fdtlistService: FdtListService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.fdtUpdateFrom = this.fb.group({
+      id : [null, Validators.required],
+      fdtName: [null, Validators.required],
+      fdtLat: [null, Validators.required],
+      fdtLng: [null, Validators.required],
+    });
+    // request to get all fdtlist
+    this.fdtlistService.getAllFdt().subscribe((data: any) => {
+      this.isLoading = true;
+      this.fdtlist = data;
+      this.isLoading = false;
+    });
   }
 
+  editFdt(data: Fdtlist): void {
+    this.visible = true;
+    this.fdtUpdateFrom?.patchValue({
+      id : data.id,
+      fdtName: data.fdtName,
+      fdtLat: data.fdtLat,
+      fdtLng: data.fdtLng,
+    });
+  }
+
+  handleCancel() {
+    this.visible = false;
+  }
+
+  handleOk() {
+    this.fdtlistService.updateFdt(this.fdtUpdateFrom.value).subscribe((data: any) => {
+      this.visible = false;
+      this.ngOnInit();     
+    })
+  }
 }
